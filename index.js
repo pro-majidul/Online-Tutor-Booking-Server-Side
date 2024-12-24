ead75a35617f29ee0be5bf2131b2aad4f2472b6d
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-12-25 03:59:57 +0600

    booked tutorials added

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 
 // middleware
 app.use(cors({
-    origin: ['http://localhost:5173'],
-    Credential: true
+    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    credentials: true,
 }))
+// app.use(cors())
 app.use(express.json())
 
 // verification middleware
@@ -41,10 +42,25 @@ async function run() {
     try {
 
         const tutorialCollection = client.db('Tutor-Booking').collection('tutors')
-        const languagelCollection = client.db('Tutor-Booking').collection('language')
+        const languagelCollection = client.db('Tutor-Booking').collection('language');
+        const tutorBookCollecton = client.db('Tutor-Booking').collection('tutorBooked')
 
+        // Tutors APIs
         app.get('/tutors', async (req, res) => {
-            const result = await tutorialCollection.find().toArray()
+            const lang = req.query.language;
+            let query = {};
+            if (lang) {
+                query = { language: lang }
+            }
+            const result = await tutorialCollection.find(query).toArray()
+
+            res.send(result)
+        })
+
+        app.get('/tutors/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await tutorialCollection.findOne(query)
             res.send(result)
         })
 
@@ -54,6 +70,7 @@ async function run() {
             res.send(result)
         })
 
+        // Language APIs
         app.post('/language', async (req, res) => {
             const data = req.body
             const result = await languagelCollection.insertOne(data);
@@ -61,7 +78,27 @@ async function run() {
         })
         app.get('/language', async (req, res) => {
             const result = await languagelCollection.find().toArray()
+            res.send(result)
         })
+
+        //Booked Tutors APIs
+        app.post('/tutorBooked', async (req, res) => {
+            const data = req.body;
+            const id = { tutorId: data.tutorId }
+            const isAxist = await tutorBookCollecton.findOne(id)
+            if (isAxist) {
+                return res.status(401).send('You already Booked This Data')
+            }
+            const result = await tutorBookCollecton.insertOne(data);
+            res.send(result)
+        })
+
+        // app.get('/tutorBooked', async (req, res) => {
+        //     const email = req.query.email;
+        //     const query = { email }
+        //     const result = await tutorBookCollecton.find(query).toArray();
+        //     res.send(result)
+        // })
 
 
         // Connect the client to the server	(optional starting in v4.7)
